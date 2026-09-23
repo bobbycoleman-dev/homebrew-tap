27116061a3f39157f3c05c2a33fdd4d2eaebcb2f
Bobby Coleman <bobbycoleman.dev@gmail.com>
2026-09-22 19:00:52 -0500

    oxide-terminal 0.6.0

--- Casks/oxide-terminal.rb
+++ Casks/oxide-terminal.rb
@@ -1,6 +1,6 @@
 cask "oxide-terminal" do
-  version "0.5.8"
-  sha256 "273fad77b2b513fa609b7abca159f40ee39813f21acf8e7eed70128aa471f2f8"
+  version "0.6.0"
+  sha256 "9348c053aac26136ffb39045a0454c65a17ada1b48397d78542ceca1cbeb0dd4"
 
   url "https://github.com/bobbycoleman-dev/oxide/releases/download/v#{version}/Oxide-#{version}.dmg"
   name "Oxide"
