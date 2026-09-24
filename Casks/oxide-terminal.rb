32a86f3c96628e9b3ed1e7b271cea5bff5197e4a
Bobby Coleman <bobbycoleman.dev@gmail.com>
2026-09-24 18:39:56 -0500

    move to oxide-temrinal org

--- Casks/oxide-terminal.rb
+++ Casks/oxide-terminal.rb
@@ -2,7 +2,7 @@ cask "oxide-terminal" do
   version "0.6.1"
   sha256 "3acc5d7999c7680db11858ee353fd0844cddadd2f2a82e12a7cf2fe48e1fb1ff"
 
-  url "https://github.com/bobbycoleman-dev/oxide/releases/download/v#{version}/Oxide-#{version}.dmg"
+  url "https://github.com/oxide-terminal/oxide/releases/download/v#{version}/Oxide-#{version}.dmg"
   name "Oxide"
   desc "GPU-rendered terminal emulator written in Rust"
   homepage "https://oxideterminal.com/"
